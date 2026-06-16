87b0ed02e5565e549a0b5b0d5accd4b45cd08c72
Disha Agarwalla <dishaagarwalla260@gmail.com>
2026-06-16 11:00:18 +0530

    Initial commit - Online Auction Platform

--- client/src/components/AuctionCard.jsx
+++ client/src/components/AuctionCard.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"; 
+import { Link } from "react-router-dom";  
 import { motion } from "framer-motion";
 import { Clock, DollarSign, User, Eye } from "lucide-react";
 
